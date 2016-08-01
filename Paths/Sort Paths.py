f0f47a0469155ee6a38d6df8e08a3d37b6c6ab44
Lasse Fister <commander@graphicore.de>
2016-07-31 18:09:58 -0700

    Make Sort Path independent of point order

--- Paths/Sort Paths.py
+++ Paths/Sort Paths.py
@@ -1,7 +1,17 @@
 #MenuTitle: Sort Paths
 # -*- coding: utf-8 -*-
 __doc__="""
-Sort Paths on all layers to make master compatible again
+Sort Paths on all layers.
+
+If Correct Path Correction broke it, this may be able to repair it again.
+This seems pretty save to run. I.e. if you apply it to all glyphs and all
+layers, it seems to not make things worse.
+
+This also sorts by signature, the implication is that, if your contours have
+wrong starting points between layers, the sort result may not be interpolation
+compatible.
+
+Current Best Pracice: run after "Correct Path Direction all Layers"
 """
 
 # It's quite ironic that we need boilerplate code to import boilerplate.py:
@@ -22,6 +32,16 @@ def magnitude(path):
         counts[n.type] += 1;
 
     signature = tuple(len(s) for s in path.segments)
+    # by "sorting" the rotations of signature, we become independent from
+    # the actual order of the points. Its irrelevant if we pick the biggest
+    # signature or the smallest, it's only important that we pick always
+    # the same signature. ALSO: some or all rotations may be identical.
+    biggestSig = signature
+    for i in range(1, len(signature)):
+        testSig = signature[i:] + signature[0:i]
+        if testSig > biggestSig:
+            biggestSig = testSig
+    signature = biggestSig
 
     return (
         0 - len(path) # most points
